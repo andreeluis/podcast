922f1f9a2203c41b267f126b2f46e612fa25d8e8
André Luís <andreluis2442@gmail.com>
2022-01-27 22:16:04 -0300

    Starting project

---
+++ Podcast.Backend/Podcast.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ServiceModel.Syndication;
+
+namespace Podcast.Backend
+{
+    internal class Podcast
+    {
+        
+    }
+
+    public class SetPodcastProgram
+    {
+        
+        public static string GetFeed()
+        {
+            string sinapseURL = "https://anchor.fm/s/814b18c/podcast/rss";
+            string nerdcastURL = "https://jovemnerd.com.br/feed-nerdcast";
+
+            return sinapseURL;
+        }
+        
+    }
+}
---
+++ Podcast.Backend/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Podcast.Backend
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Bem vindo! Escolha qual feed quer ver:");
+            Console.WriteLine("[01] Sinapse ;");
+            Console.WriteLine("[02] Nerdcast ;");
+
+            int idFeed = Int16.Parse(Console.ReadLine());
+            Console.Write(idFeed);
+            Console.WriteLine(SetPodcastProgram.GetFeed());
+        }
+    }
+}
